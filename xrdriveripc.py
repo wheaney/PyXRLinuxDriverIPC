6f1844829e11fcc7664398d4c95d8fbdb6341669
wheaney <42350981+wheaney@users.noreply.github.com>
2026-01-20 14:12:35 -0800

    Add support for deadzone threshold config

--- xrdriveripc.py
+++ xrdriveripc.py
@@ -87,6 +87,7 @@ CONFIG_ENTRIES = {
     'smooth_follow_track_yaw': [parse_boolean, True],
     'neck_saver_horizontal_multiplier': [parse_float, 1.0],
     'neck_saver_vertical_multiplier': [parse_float, 1.0],
+    'dead_zone_threshold_deg': [parse_float, 0.0],
     'opentrack_app_ip': [parse_string, '127.0.0.1'],
     'opentrack_app_port': [parse_int, 4242],
     'opentrack_listener_enabled': [parse_boolean, False],
